42f6ead3dc600492eb28a826e7744286a07e3805
danielspector <danielyspector@gmail.com>
2014-04-01 10:23:39 -0400

    added jquery functions

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,5 +1,51 @@
 $(document).ready(function(){
 
-  alert("loaded on page");
+  var pv = $("#present_value").val();
+  var pmt = $("#pmt").val();
+  var age = $("#age").val();
+  var expenseRatio = $("#expense_ratio").val();
+  
+  function PortfolioValue (PV, pmt, age, ER) {
+    this.pv = (PV);
+    this.pmt = (pmt);
+    this.age = (age);
+    this.ER = (ER);
+
+    this.numberOfPeriods = monthsUntilRetirement(this.age);
+    this.interestRate = monthlyAdjustInterestRate(8 - this.ER)/100;
+    this.FVofPV = calcFVofPV(this.pv, this.numberOfPeriods, this.interestRate);
+    this.FVofOrdinaryAnnuity = calcFVofOrdinaryAnnuity(this.pmt, this.numberOfPeriods, this.interestRate)
+    this.fv = calcFVofOrdinaryAnnuityWithPV(this.FVofPV, this.FVofOrdinaryAnnuity);
+  }
+
+  $(".user_input").on("submit", function(e){
+    e.preventDefault;
+    var port1 = new PortfolioValue(pv, pmt, age, expenseRatio)
+    console.log(port1.fv);
+  });
 
 });
+
+// function calcFVofOrdinaryAnnuity (pmt, numberOfPeriods, monthlyInterestRate) {
+//   var factor = ((Math.pow((1 + monthlyInterestRate), numberOfPeriods)) - 1) / monthlyInterestRate;
+//   var futureValueofOrdinaryAnnuity = pmt * factor;
+//   return futureValueofOrdinaryAnnuity;
+// }
+
+// function calcFVofPV (pv, numberOfPeriods, monthlyInterestRate){
+//   var factor = Math.pow((1 + monthlyInterestRate), numberOfPeriods)
+//   var pv = pv;
+//   return futureValueofPV = pv * factor;
+// }
+
+// function calcFVofOrdinaryAnnuityWithPV(FVofPV, FVofOrdinaryAnnuity) {
+//   return FVofPV + FVofOrdinaryAnnuity;
+// }
+
+// function monthlyAdjustInterestRate(interestRate) {
+//   return interestRate / 12
+// }
+
+// function monthsUntilRetirement(age){
+//   return (65 - age) * 12 
+// }
