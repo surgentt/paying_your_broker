ae6f2dae7c5404904eb6b899a0b9e7be74fe1af5
danielspector <danielyspector@gmail.com>
2014-04-01 19:51:56 -0400

    combined two forms

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -1,9 +1,17 @@
 class FundsController < ApplicationController
   def create
-    fund_ticker = params[:fund_ticker]
     @present_value = params[:present_value]
     @pmt = params[:pmt]
     @age = params[:age]
-    @exp_ratio = Fund.new.get_expense(fund_ticker)
+    
+    if params[:fund_ticker].empty?
+      @exp_ratio = params[:expense_ratio]
+    else
+      @exp_ratio = Fund.new.get_expense(params[:fund_ticker])
+    end
+
+    respond_to do |format|
+      format.js
+    end
   end
 end
