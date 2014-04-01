50e8f581da6df16ee448b859cadd54a00987a597
Thomas Surgent <surgentt@gmail.com>
2014-03-31 20:50:12 -0400

    calculation functions appear to be in order

--- app/assets/javascripts/calc.js
+++ app/assets/javascripts/calc.js
@@ -1,15 +1,23 @@
 function calcFVofOrdinaryAnnuity (pmt, numberOfPeriods, monthlyInterestRate) {
-    var factor = ((Math.pow((1 + monthlyInterestRate), numberOfPeriods)) - 1) / monthlyInterestRate;
-    var futureValueofOrdinaryAnnuity = pmt * factor;
-    return futureValueofOrdinaryAnnuity;
+  var factor = ((Math.pow((1 + monthlyInterestRate), numberOfPeriods)) - 1) / monthlyInterestRate;
+  var futureValueofOrdinaryAnnuity = pmt * factor;
+  return futureValueofOrdinaryAnnuity;
 }
 
-function calcFVofPV (pv, numberOfPeriods, interestRate){
-  var factor = Math.pow((1 + interestRate), numberOfPeriods)
+function calcFVofPV (pv, numberOfPeriods, monthlyInterestRate){
+  var factor = Math.pow((1 + monthlyInterestRate), numberOfPeriods)
   var pv = pv;
   return futureValueofPV = pv * factor;
 }
 
 function calcFVofOrdinaryAnnuityWithPV(FVofPV, FVofOrdinaryAnnuity) {
   return FVofPV + FVofOrdinaryAnnuity;
+}
+
+function monthlyAdjustInterestRate(interestRate) {
+  return interestRate / 12
+}
+
+function yearUntilRetirement(age){
+  return (65 - age) * 12 
 }
--- spec/javascripts/calc_spec.js
+++ spec/javascripts/calc_spec.js
@@ -1,26 +1,40 @@
 describe ("future value", function(){
   var numberOfPeriods;
-  var interestRate;
+  var monthlyinterestRate;
   var pv;
   var pmt;
   beforeEach(function(){
     numberOfPeriods = 480;
-    interestRate = 0.00666666667;
+    monthlyInterestRate = 0.00666666667;
     pv = 200;
     pmt = 100;
   });
 
   it ("should return the FV of an annuity", function(){
-    expect(calcFVofOrdinaryAnnuity(pmt, numberOfPeriods, interestRate)).toBeCloseTo(349101, 0)
+    expect(calcFVofOrdinaryAnnuity(pmt, numberOfPeriods, monthlyInterestRate)).toBeCloseTo(349101, 0)
   });
 
   it ("should return the FV of a present value", function (){
-    expect(calcFVofPV(pv, numberOfPeriods, interestRate)).toBeCloseTo(4855, 0)
+    expect(calcFVofPV(pv, numberOfPeriods, monthlyInterestRate)).toBeCloseTo(4855, 0)
   });
 
   it ("should return the FV of an ordinary annuity when theres a present sum", function(){
-    var FVofPV = calcFVofPV(pv, numberOfPeriods, interestRate)
-    var FVofOrdinaryAnnuity = calcFVofOrdinaryAnnuity(pmt, numberOfPeriods, interestRate)
+    var FVofPV = calcFVofPV(pv, numberOfPeriods, monthlyInterestRate)
+    var FVofOrdinaryAnnuity = calcFVofOrdinaryAnnuity(pmt, numberOfPeriods, monthlyInterestRate)
     expect(calcFVofOrdinaryAnnuityWithPV(FVofPV, FVofOrdinaryAnnuity)).toBeCloseTo(353955, 0)
+  });
+
+});
+
+describe ("adjustments from form", function(){
+  it ("should adjust the interest rate", function(){
+    var interestRate = 0.08;
+    var monthlyInterestRate = 0.00666666667;
+    expect(monthlyAdjustInterestRate(interestRate)).toBeCloseTo(monthlyInterestRate, 3)
+  });
+
+  it ("should return number of periods until retirement", function(){
+    var age = 23
+    expect(yearUntilRetirement(age)).toEqual(504)
   });
 });
