75a6dbd7121fc333454249aed8620d24f2c1c19e
Thomas Surgent <surgentt@gmail.com>
2014-04-01 09:43:23 -0400

    small changes

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -6,7 +6,7 @@ describe "getting a amount fucked by Banker" do
     visit root_path
     within ".user_input" do 
       fill_in "present_value", with: "300"
-      fill_in "pmt", with: "200"
+      fill_in "pmt",           with: "200"
       fill_in "age",           with: "23"
       fill_in "expense_ratio", with: "1.5"
     end
