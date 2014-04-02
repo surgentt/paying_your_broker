8f7bb3aeede7a818d5e00eaeb12815e7929fd5ae
danielspector <danielyspector@gmail.com>
2014-04-02 17:46:43 -0400

    fixed default values in the controller

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -1,10 +1,29 @@
 class FundsController < ApplicationController
 
   def create
-    @present_value = params[:present_value].gsub(",", "").to_i
-    @pmt = params[:pmt].gsub(",","").to_i
-    @age = params[:age].to_i
-    @exp_ratio = Fund.new.get_expense(params[:fund_ticker]).to_f
+    if params[:present_value].to_i == 0
+      @present_value = 10000
+    else
+      @present_value = params[:present_value].gsub(",", "").to_i
+    end
+
+    if params[:pmt].to_i == 0
+      @pmt = 1000
+    else
+      @pmt = params[:pmt].gsub(",","").to_i
+    end
+
+    if params[:age].to_i == 0
+      @age = 35
+    else
+      @age = params[:age].to_i
+    end
+    
+    if params[:fund_ticker].to_i == 0
+      @exp_ratio = 0.81
+    else 
+      @exp_ratio = Fund.new.get_expense(params[:fund_ticker]).to_f
+    end
 
     respond_to do |format|
       format.js
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   
   root 'funds#home'
-  post '/funds' => "funds#create", defaults: { present_value: "10000", pmt: "1000", age: "35", fund_ticker: "FFFFX" }
+  post '/funds' => "funds#create"
 
 end
