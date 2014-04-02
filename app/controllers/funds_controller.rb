ef5066a480af249eb9995c18e235b2d29e279da4
danielspector <danielyspector@gmail.com>
2014-04-02 18:11:40 -0400

    fixed placholder and defaults

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -1,19 +1,19 @@
 class FundsController < ApplicationController
 
   def create
-    if params[:present_value].to_i == 0 
+    if params[:present_value] == "" 
       @present_value = 10000
     else
       @present_value = params[:present_value].gsub(",", "").to_i
     end
 
-    if params[:pmt].to_i == 0
+    if params[:pmt] == ""
       @pmt = 1000
     else
       @pmt = params[:pmt].gsub(",","").to_i
     end
 
-    if params[:age].to_i == 0
+    if params[:age] == ""
       @age = 35
     else
       @age = params[:age].to_i
