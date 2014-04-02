03ffaf04660e95edfdeb1c55018b8d154704d805
danielspector <danielyspector@gmail.com>
2014-04-02 16:36:33 -0400

    created defaults for params

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -7,7 +7,6 @@ class FundsController < ApplicationController
     @present_value = params[:present_value].gsub(",", "").to_i
     @pmt = params[:pmt].gsub(",","").to_i
     @age = params[:age].to_i
-    
     @exp_ratio = Fund.new.get_expense(params[:fund_ticker]).to_f
 
     respond_to do |format|
